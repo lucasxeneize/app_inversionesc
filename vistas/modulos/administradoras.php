ca744d1137059125d9f3b70a4a606b91df0d2cf2
Lucas Nieto <lucasxeneize12@gnmail.com>
2018-08-10 15:18:24 -0400

    Cambio en titulo.

--- vistas/modulos/administradoras.php
+++ vistas/modulos/administradoras.php
@@ -12,7 +12,7 @@
       
       <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
       
-      <li class="active">Administradoras</li>
+      <li class="active">Administradoras Agentes</li>
     
     </ol>
 
